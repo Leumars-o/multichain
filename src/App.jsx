7b7dd6e066770f227dda8cd63b4fbc18bd8ffec7
Leumars <samex150@gmail.com>
2024-03-21 21:37:36 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import loadingSpinner from './components/loading-spinner'
+
+function App() {
+
+  return (
+   <body>
+    <loadingSpinner/>
+   </body>
+  )
+}
+
+export default App
---
+++ src/components/loading-spinner.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const loadingSpinner = () => {
+  return (
+    <div class="Loader">
+            <div class="spinnerPairHolder">
+                <div class="spinnerPair">
+                    <div class="spinnerPairCercle"></div>
+                    <div class="spinnerPairCercle"></div>
+                </div>
+                <div class="spinnerPair">
+                    <div class="spinnerPairCercle"></div>
+                    <div class="spinnerPairCercle"></div>
+                </div>
+                <div class="spinnerPair">
+                    <div class="spinnerPairCercle"></div>
+                    <div class="spinnerPairCercle"></div>
+                </div>
+                <div class="spinnerPair">
+                    <div class="spinnerPairCercle"></div>
+                    <div class="spinnerPairCercle"></div>
+                </div>
+                <div class="spinnerPair">
+                    <div class="spinnerPairCercle"></div>
+                    <div class="spinnerPairCercle"></div>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default loadingSpinner
