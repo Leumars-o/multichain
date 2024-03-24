5bb6271ebe17c8b9afc6ce637bbcca4dd0f46fd0
Leumars <samex150@gmail.com>
2024-03-24 14:23:27 +0100

    AGs code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import loadingSpinner from './components/loading-spinner'
+import Multii from './components/multii/Multii'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-   <body>
-    <loadingSpinner/>
-   </body>
+    <>
+      <Multii />
+    </>
   )
 }
 
